4f259224829ea23be80a4559da8f57570714eeb2
Hadi Jafari <haadijafari2003@gmail.com>
2024-09-04 21:07:55 +0330

    [FIX] Debug Toolbar Production
    [+] Solve the error message in production for debug toolbar package

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,10 +1,12 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
+from core.settings.base import DEBUG
+
+if DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from core.settings.base import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
