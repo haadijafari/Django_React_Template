d77a6efafed3e933cf8d7a96f542720aeddc4280
Hadi Jafari <haadijafari2003@gmail.com>
2024-09-04 20:50:36 +0330

    [FIX] Template Directory

--- project/core/settings/base.py
+++ project/core/settings/base.py
@@ -45,9 +45,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'frontend/vite-react/html',
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
